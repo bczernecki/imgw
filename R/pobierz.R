3b598a5c782f16615f9bdcaddff787e48ea3dcdb
bczernecki <bczernecki@gmail.com>
2017-04-05 11:40:08 +0200

    nchar zamiast length

--- R/pobierz.R
+++ R/pobierz.R
@@ -1,9 +1,9 @@
 #' Funkcja pobierająca dane z serwisu dane.imgw.pl
 #'
 #' Pozwala na zautomatyzowanie procesu pobierania danych meteorologicznych i hydrologicznych z oficjalnego repozytorium Instytutu Meteorologii i Gospodarki Wodnej - Państwowego Instytutu Badawczego (https://dane.imgw.pl)
-#' @param data_start data poczatkowa od ktorej beda pobierane dane. Obiekt klasy Date
-#' @param data_end data końcowa do ktorej beda pobierane. Obiekt klasy Date. Domyslnie funkcja rozszerzy okres pobierania o tydzien w przod i w tyl
 #' @param user_pass  login i token ze strony https://dane.imgw.pl. Zmienna w postaci email:token, np. bczernecki@gmail.com:68a60056
+#' @param data_start data początkowa od której beda pobierane dane. Obiekt klasy Date
+#' @param data_end data końcowa do ktorej beda pobierane. Obiekt klasy Date. Domyslnie funkcja rozszerzy okres pobierania o tydzien w przod i w tyl
 #' @param stacja kod stacji wg strony https://dane.imgw.pl
 #' @param kod kod pobieranego elementu meteorologicznego/hydrologicznego
 
@@ -13,9 +13,9 @@
 #' @examples
 #' # wymagane ustalenie ponizszych parametrow pobierania:
 #' 
+#' user_pass <- "" 
 #' data_start <- "2016-12-26" 
 #' data_end <- "2017-02-20" 
-#' user_pass <- "" 
 #' stacja <- "352160330" # przykladowe id dla Poznania; numer mozna pobrac ze strony https://dane.imgw.pl
 #' kod <- "B100B008CD" # srednia dobowa temp. powietrza synop; j.w.
 #' pobierz(data_start,data_end,user_pass,stacja,kod)
@@ -23,8 +23,8 @@
 
 pobierz <- function(data_start,data_end,user_pass,stacja,kod){
   
-    if(length(user_pass)<5) print("sprawdz argument: user_pass")
-    stopifnot(length(user_pass)>5)
+    if(nchar(user_pass)<=0) print("sprawdz argument: user_pass")
+    stopifnot(nchar(user_pass)>0)
   
 
   daty <- seq.Date(from=as.Date(data_start)-7, to=as.Date(data_end)+7, by="7 days")
