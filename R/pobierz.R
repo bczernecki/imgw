a2fbde1c29f8d3d36776a76dcdd9bd14a814a6c6
bczernecki <bczernecki@gmail.com>
2017-05-01 01:56:59 +0200

    deal_with_errors1

--- R/pobierz.R
+++ R/pobierz.R
@@ -1,11 +1,14 @@
 #' Pobieranie danych meteorologicznych z serwisu IMGW-PIB (https://dane.imgw.pl)
 #'
-#' Funkcja pozwalająca na zautomatyzowanie procesu pobierania danych meteorologicznych i hydrologicznych z oficjalnego repozytorium Instytutu Meteorologii i Gospodarki Wodnej - Państwowego Instytutu Badawczego - https://dane.imgw.pl
+#' Funkcja pozwalająca na zautomatyzowanie procesu pobierania danych meteorologicznych i hydrologicznych z oficjalnego repozytorium Instytutu Meteorologii i Gospodarki Wodnej - Państwowego Instytutu Badawczego - https://dane.imgw.pl.
+#' Z uwagi na ograniczenia związane z liczbą tworzonych zapytań (ok. 10 tys. na 10 min.) funkcja może wymuszać krótkie przerwy w pobieraniu w celu odświeżenia limitów.
+#' 
 #' @param user_pass  Login i token wygenerowany przez serwis https://dane.imgw.pl. Parametr w postaci email:token, np. bczernecki@gmail.com:68a60055
 #' @param data_start Data początkowa od której będą pobierane dane. Obiekt klasy Date
 #' @param data_end Data końcowa do której będą pobierane. Obiekt klasy Date. Domyślnie funkcja rozszerza zakres pobierania danych o tydzień w przód i w tył
 #' @param stacja Kod stacji wg strony https://dane.imgw.pl
 #' @param kod Kod pobieranego elementu meteorologicznego/hydrologicznego
+#' @param podglad Wyświetla szczegółowe informacje cząstkowe (link oraz pierwsze kilkanaście znaków) w trakcie pobierania (TRUE/FALSE). Domyślnie FALSE 
 
 #' @importFrom RCurl getURL
 #'
@@ -21,7 +24,7 @@
 #' pobierz(data_start,data_end,user_pass,stacja,kod)
 
 
-pobierz <- function(data_start,data_end,user_pass,stacja,kod){
+pobierz <- function(data_start,data_end,user_pass,stacja,kod, podglad=FALSE){
   library(RCurl)
   options(RCurlOptions = list(cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))) 
   
@@ -38,44 +41,43 @@ pobierz <- function(data_start,data_end,user_pass,stacja,kod){
       
       link <- paste0("https://dane.imgw.pl/1.0/pomiary/cbdh/",
                     stacja,"-",kod,"/tydzien/",daty[i],"?format=csv")
-  
-      a <-gsub(
-        getURL(link,userpwd = user_pass, cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl")),
-        pattern = "\\r",replacement = "")
+      
+      a <- tryCatch(getURL(link,userpwd = user_pass, cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl")), error=function(e) 0)
+      a <-gsub(a,  pattern = "\\r",replacement = "")
+      
+      if(any(grepl("429,",a))) {
+        cat(paste("Przekroczono limit pobierania. Czekam 5 min. przed ponowną próbą\n"))
+        cat(paste(link))
+        cat("Komunikat ze strony:\t",paste(a))
+        Sys.sleep(300)
+        a <- tryCatch(getURL(link,userpwd = user_pass, cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl")), error=function(e) 0)
+        a <-gsub(a,  pattern = "\\r",replacement = "")
+      }
       
       if(any(grepl("Unauthorized", a))) {
-        cat(paste("Wystąpił problem z autoryzacją...\n [1] Jeśli wykonałeś(aś) >10tys. pobrań odczekaj >10 min.\n [2] Jeśli nie - sprawdź czy link:\n", link[1] , " działa poprawnie w przeglądarce"))
-        
-        
-          n <- readline(prompt="\nAby kontynuować pobieranie wpisz w konsoli 1 (lub cokolwiek innego).\nAby odczekać 15 min. wpisz 2\n Aby przerwać wpisz 3\n")
+        cat(paste("Wystąpił problem z autoryzacją. Sprawdź nazwę użytkownika i hasło oraz sprawdź czy link:\n", link[1] , " działa poprawnie w przeglądarce"))
+        stop()
+        }  
           
-          
-          if(n=="2") {
-            cat(paste("Wybrane opcję nr 2 - odczekanie 15 min. \t", Sys.time()))
-            Sys.sleep(900)
-          }
+        if(podglad==TRUE){
+        print(link)
+        print(substr(a,1 , 60))
+        }
         
-          if(n=="3"){
-            cat(paste("Wybrane opcję nr 3 - przerwanie"))
-            stop()
-          }  
-          
-      } else {
-          
-        
-  
-      a <- read.csv(textConnection(a), sep=";", 
+        if(nchar(a)>1){
+          a <- read.csv(textConnection(a), sep=";", 
                 colClasses = c("POSIXct","numeric","numeric"))
-  
-    wynik <- rbind.data.frame(wynik, a)
+          wynik <- rbind.data.frame(wynik, a)
     
-    cat(paste0(stacja,"\t", daty[i],"\t" ,round((i/length(daty))*100,1),"%\n"))
-      
-    } # koniec if-else
-      
+          cat(paste0(stacja,"\t", daty[i],"\t" ,round((i/length(daty))*100,1),"%\n"))
+        } else {
+          cat(paste("Wystąpił błąd\tdata:", link))
+          cat(paste("Pobieranie będzie kontynuowane dla kolejnych dat za 1 min."))
+          Sys.sleep(60)
+        } # koniec if-else dla nchar(a)>1
+    
     i <- i+1
-
-}
+} # koniec petli while
 
 
 return(wynik)
