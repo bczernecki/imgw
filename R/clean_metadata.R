2b5d802ab768b80126153429f44086aefc338711
Bartosz <nwp@amu.edu.pl>
2019-04-04 13:26:21 +0200

    dodanie suppressWarnings()

--- R/clean_metadata.R
+++ R/clean_metadata.R
@@ -18,7 +18,7 @@
 #'
 
 clean_metadata <- function(adres, rzad = "synop", interwal = "terminowe"){
-  a <- na.omit(read.fwf2(adres, widths = c(1000), fileEncoding = "CP1250", stringsAsFactors = FALSE))
+  a <- suppressWarnings(na.omit(read.fwf(adres, widths = c(1000), fileEncoding = "CP1250", stringsAsFactors = FALSE)))
   doilu <- max(nchar(a$V1), na.rm = TRUE)
 
   if(rzad == "opad") doilu <- 40 # wyjatek dla opadow
@@ -30,8 +30,8 @@ clean_metadata <- function(adres, rzad = "synop", interwal = "terminowe"){
     pola <- substr(a$V1, doilu - 3, doilu + 2)
   }
 
-  a$pole1 <- as.numeric(unlist(lapply(strsplit(pola, "/"), function(x) x[1])))
-  a$pole2 <- as.numeric(unlist(lapply(strsplit(pola, "/"), function(x) x[2])))
+  a$pole1 <- suppressWarnings(as.numeric(unlist(lapply(strsplit(pola, "/"), function(x) x[1]))))
+  a$pole2 <- suppressWarnings(as.numeric(unlist(lapply(strsplit(pola, "/"), function(x) x[2]))))
 
   a$V1 <- trimws(substr(a$V1, 1, nchar(a$V1) - 3))
   #a <- a[nchar(a$V1)>2,] # usuwamy puste lub prawie puste wiersze dodatkowo...
