e3417dac302e05609001a2f31630cae6050e46b4
jn <tupiszakaczki@gmail.com>
2019-04-06 13:01:10 +0200

    improves code style

--- R/metadane.R
+++ R/metadane.R
@@ -17,21 +17,28 @@
 metadane <- function(interwal, rzad){ # interwal moze byc: miesieczne, dobowe, terminowe
   b <- NULL
 
+  base_url <- "https://dane.imgw.pl/data/dane_pomiarowo_obserwacyjne/"
+
   # METADANE DOBOWE:
   if(interwal == "dobowe")   { # uwaga! dobowe maja dla klimatow i synopow po 2 pliki z metadanymi!!!
 
     if(rzad == "synop"){
-      b[[1]] <- clean_metadata(adres = "https://dane.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/dobowe/synop/s_d_format.txt", rzad = "synop", interwal = "dobowe")
-      b[[2]] <- clean_metadata(adres = "https://dane.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/dobowe/synop/s_d_t_format.txt", rzad = "synop", interwal = "dobowe")
+      b[[1]] <- clean_metadata(adres = paste0(base_url, "dane_meteorologiczne/dobowe/synop/s_d_format.txt"),
+                               rzad = "synop", interwal = "dobowe")
+      b[[2]] <- clean_metadata(adres = paste0(base_url, "dane_meteorologiczne/dobowe/synop/s_d_t_format.txt"),
+                                              rzad = "synop", interwal = "dobowe")
     }
 
     if(rzad == "klimat"){
-      b[[1]] <- clean_metadata(adres = "https://dane.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/dobowe/klimat/k_d_format.txt", rzad = "klimat", interwal = "dobowe")
-      b[[2]] <- clean_metadata(adres = "https://dane.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/dobowe/klimat/k_d_t_format.txt", rzad = "klimat", interwal = "dobowe")
+      b[[1]] <- clean_metadata(adres = paste0(base_url, "dane_meteorologiczne/dobowe/klimat/k_d_format.txt"),
+                               rzad = "klimat", interwal = "dobowe")
+      b[[2]] <- clean_metadata(adres = paste0(base_url, "dane_meteorologiczne/dobowe/klimat/k_d_t_format.txt"),
+                               rzad = "klimat", interwal = "dobowe")
     }
 
     if(rzad == "opad"){
-      b[[1]] <- clean_metadata(adres = "https://dane.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/dobowe/opad/o_d_format.txt", rzad = "opad", interwal = "dobowe")
+      b[[1]] <- clean_metadata(adres = paste0(base_url, "dane_meteorologiczne/dobowe/opad/o_d_format.txt"),
+                               rzad = "opad", interwal = "dobowe")
     }
 
   } # koniec "DOBOWE"
@@ -43,17 +50,22 @@ metadane <- function(interwal, rzad){ # interwal moze byc: miesieczne, dobowe, t
   if(interwal == "miesieczne") {
 
     if(rzad == "synop"){
-      b[[1]] <- clean_metadata("https://dane.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/miesieczne/synop/s_m_d_format.txt", rzad = "synop", interwal = "miesieczne")
-      b[[2]] <- clean_metadata("https://dane.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/miesieczne/synop/s_m_t_format.txt", rzad = "synop", interwal = "miesieczne")
+      b[[1]] <- clean_metadata(paste0(base_url, "dane_meteorologiczne/miesieczne/synop/s_m_d_format.txt"),
+                               rzad = "synop", interwal = "miesieczne")
+      b[[2]] <- clean_metadata(paste0(base_url, "dane_meteorologiczne/miesieczne/synop/s_m_t_format.txt"),
+                               rzad = "synop", interwal = "miesieczne")
     }
 
     if(rzad == "klimat"){
-      b[[1]] <- clean_metadata("https://dane.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/miesieczne/klimat/k_m_d_format.txt", rzad = "klimat", interwal = "miesieczne")
-      b[[2]] <- clean_metadata("https://dane.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/miesieczne/klimat/k_m_t_format.txt", rzad = "klimat", interwal = "miesieczne")
+      b[[1]] <- clean_metadata(paste0(base_url, "dane_meteorologiczne/miesieczne/klimat/k_m_d_format.txt"),
+                               rzad = "klimat", interwal = "miesieczne")
+      b[[2]] <- clean_metadata(paste0(base_url, "dane_meteorologiczne/miesieczne/klimat/k_m_t_format.txt"),
+                               rzad = "klimat", interwal = "miesieczne")
     }
 
     if(rzad == "opad"){
-      b[[1]] <- clean_metadata("https://dane.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/miesieczne/opad/o_m_format.txt", rzad = "opad", interwal = "miesieczne")
+      b[[1]] <- clean_metadata(paste0(base_url, "dane_meteorologiczne/miesieczne/opad/o_m_format.txt"),
+                               rzad = "opad", interwal = "miesieczne")
     }
 
   } # koniec MIESIECZNYCH
@@ -61,8 +73,10 @@ metadane <- function(interwal, rzad){ # interwal moze byc: miesieczne, dobowe, t
 
   ## rozpoczecie dla danych TERMINOWYCH:
   if(interwal == "terminowe"){
-    if(rzad == "synop") b[[1]] <- clean_metadata("https://dane.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/terminowe/synop/s_t_format.txt", rzad = "synop", interwal = "terminowe")
-    if(rzad == "klimat") b[[1]] <- clean_metadata("https://dane.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/terminowe/klimat/k_t_format.txt", rzad = "klimat", interwal = "terminowe")
+    if(rzad == "synop") b[[1]] <- clean_metadata(paste0(base_url, "dane_meteorologiczne/terminowe/synop/s_t_format.txt"),
+                                                 rzad = "synop", interwal = "terminowe")
+    if(rzad == "klimat") b[[1]] <- clean_metadata(paste0(base_url, "dane_meteorologiczne/terminowe/klimat/k_t_format.txt"),
+                                                  rzad = "klimat", interwal = "terminowe")
   }
 
   return(b)
