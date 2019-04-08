2b58a9540770add55d1fdbbf00ce16adb3df65e6
Arkadiusz Głogowski <arkadiusz.glogowski@upwr.edu.pl>
2019-04-08 08:34:40 +0200

    brak uwzglednienia kontroli dla 2017 roku (ostatni rok pomiarów) inna struktura pliku

---
+++ R/hydro_miesieczne.R
@@ -0,0 +1,57 @@
+#' Pobranie danych hydrologicznych dla miesiecznego okresu, udostepnionych w zbiorze danepubliczne.imgw.pl
+#'
+#'
+#' @param rok wektor dla wybranych lat (np. 1966:2000)
+#' @param coords czy dodac wspolrzedne (WGS84) i wysokosc stacji? (domyslnie coords = FALSE)
+#' @import RCurl XML
+#' @importFrom utils download.file unzip read.csv
+#' @return
+#' @export
+#'
+#' @examples \dontrun{
+#'
+#' }
+#'
+# Stan wody 9999 oznacza brak danych w bazie lub przerwy w obserwacjach w danym miesiącu i stad brak możliwości obliczenia charakterystyk.
+#Przepływ 99999.999 oznacza brak danych lub przerwy w obserwacjach w danym miesiacu i stad brak możliwości obliczenia charakterystyk.
+#Temperatura wody 99.9 oznacza brak danych lub przerwy w obserwacjach w danym miesiacu i stad brak możliwości obliczenia charakterystyk.
+# Działa tylko dla miesiecznych hydro
+hydro_miesieczne = function(rok = 1966:2000,coords=FALSE){
+  base_url <- "https://dane.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_hydrologiczne/"
+  interwal="miesieczne"
+  a <- getURL(paste0(base_url, interwal, "/"),
+              ftp.use.epsv = FALSE,
+              dirlistonly = TRUE)
+
+  ind <- grep(readHTMLTable(a)[[1]]$Name, pattern = "/")
+  katalogi <- as.character(readHTMLTable(a)[[1]]$Name[ind])
+  katalogi <-gsub(x = katalogi, pattern = "/", replacement = "")
+  adres_meta=paste0(base_url,interwal,"/mies_info.txt")
+  meta <- hydro_clean_metadata(adres_meta,interwal)
+
+
+
+  calosc <- vector("list", length = length(katalogi))
+  for (i in seq_along(katalogi)){
+    katalog=katalogi[i]
+    print(i)
+
+    adres <- paste0(base_url, interwal, "/", katalog, "/mies_", katalog, ".zip")
+
+    temp <- tempfile()
+    temp2 <- tempfile()
+    download.file(adres, temp)
+    unzip(zipfile = temp, exdir = temp2)
+    plik1 <- paste(temp2, dir(temp2), sep = "/")[1]
+    data1 <- read.csv(plik1, header = FALSE, stringsAsFactors = FALSE, fileEncoding = "CP1250")
+    colnames(data1) <- meta
+    calosc[[i]] <-data1
+  }
+  calosc <- do.call(rbind, calosc)
+  # wyjatki na brak stanu, przeplywu i temperatury
+  calosc[calosc==9999]<-NA
+  calosc[calosc==99999.999]<-NA
+  calosc[calosc==99.9]<-NA
+  calosc <- calosc[calosc$`Rok hydrologiczny` %in% rok, ]
+  return(calosc)
+}
