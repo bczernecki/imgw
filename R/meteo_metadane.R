d53e3e0b842c24020d0a3b39e4f0167dc0265014
unknown <arkadiusz.glogowski@upwr.edu.pl>
2019-04-10 10:07:51 +0200

    English version

--- R/meteo_metadane.R
+++ R/meteo_metadane.R
@@ -1,88 +1,87 @@
 #' Meteorological metadata
 #'
-#' Pobranie opisu (metadanych) do danych meteorologicznych  udostępnionych w repozytorium danepubliczne.imgw.pl
-#' Domyślnie funkcja zwraca listę bądź ramkę danych dla wybranego podzbioru (w zależności od radzaj)
-#`
-#' @param interwal -  Argument przyjmujący wartość: 'terminowe' , dobowe' (domyślnie), lub 'miesieczne'.  Oznacza dla jakiej rozdzielczosci czasowej maja zostac przygotowane metadane dla srodowiska R
-#' @param rzad -  Argument przyjmujący wartość: 'synop' (domyślnie) lub 'klimat'.  Oznacza dla jakiego rzadu/rzędu staji mają zostać przygotowane metadane.
+#' Downloading the description (metadata) to the meteorological data available in the danepubliczne repository.imgw.pl
+#' By default, the function returns a list or data frame for a selected subset (depending on how to cop#`
+#'
+#' @param interval -  Argument accepting the value 'terminowe' , dobowe' (default), or 'miesieczne'.  Indicates for which time resolution metadata for environment R should be prepared
+#' @param rank rank of station ("synop" , "klimat" , "opad")
 #'
 #' @export
 #' @examples
-#' # przykladowe uzycie:
 #' \dontrun{
-#' meta <- meteo_metadane(interwal = "dobowe", rzad = "synop")
-#' meta <- meteo_metadane(interwal = "terminowe", rzad = "synop")
-#' meta <- meteo_metadane(interwal = "miesieczne", rzad = "opad")
+#' meta <- meteo_metadane(interval = "dobowe", rank = "synop")
+#' meta <- meteo_metadane(interval = "terminowe", rank = "synop")
+#' meta <- meteo_metadane(interval = "miesieczne", rank = "opad")
 #' }
 
-meteo_metadane <- function(interwal, rzad){ # interwal moze byc: miesieczne, dobowe, terminowe
+meteo_metadane <- function(interval, rank){ # interval moze byc: miesieczne, dobowe, terminowe
   b <- NULL
 
   base_url <- "https://dane.imgw.pl/data/dane_pomiarowo_obserwacyjne/"
 
   # METADANE DOBOWE:
-  if(interwal == "dobowe")   { # uwaga! dobowe maja dla klimatow i synopow po 2 pliki z metadanymi!!!
+  if(interval == "dobowe")   { # uwaga! dobowe maja dla klimatow i synopow po 2 pliki z metadanymi!!!
 
-    if(rzad == "synop"){
+    if(rank == "synop"){
       b[[1]] <- clean_metadata_meteo(adres = paste0(base_url, "dane_meteorologiczne/dobowe/synop/s_d_format.txt"),
-                               rzad = "synop", interwal = "dobowe")
+                               rank = "synop", interval = "dobowe")
       b[[2]] <- clean_metadata_meteo(adres = paste0(base_url, "dane_meteorologiczne/dobowe/synop/s_d_t_format.txt"),
-                                              rzad = "synop", interwal = "dobowe")
+                                              rank = "synop", interval = "dobowe")
     }
 
-    if(rzad == "klimat"){
+    if(rank == "klimat"){
       b[[1]] <- clean_metadata_meteo(adres = paste0(base_url, "dane_meteorologiczne/dobowe/klimat/k_d_format.txt"),
-                               rzad = "klimat", interwal = "dobowe")
+                               rank = "klimat", interval = "dobowe")
       b[[2]] <- clean_metadata_meteo(adres = paste0(base_url, "dane_meteorologiczne/dobowe/klimat/k_d_t_format.txt"),
-                               rzad = "klimat", interwal = "dobowe")
+                               rank = "klimat", interval = "dobowe")
     }
 
-    if(rzad == "opad"){
+    if(rank == "opad"){
       b[[1]] <- clean_metadata_meteo(adres = paste0(base_url, "dane_meteorologiczne/dobowe/opad/o_d_format.txt"),
-                               rzad = "opad", interwal = "dobowe")
+                               rank = "opad", interval = "dobowe")
     }
 
   } # koniec "DOBOWE"
 
 
   # TODO: pod adresem: https://dane.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/miesieczne/synop/
-  # sa 2 rzade metadanych, bo pliki miesieczne maja 2 rzade danych; w starej wersji paczki tylko jedna wersja jest uwzgledniana
-  # dodatkowo inne rzade danych beda do pobrania w zaleznosci od danych SYNOP, KLIMAT, OPAD:
-  if(interwal == "miesieczne") {
+  # sa 2 ranke metadanych, bo pliki miesieczne maja 2 ranke danych; w starej wersji paczki tylko jedna wersja jest uwzgledniana
+  # dodatkowo inne ranke danych beda do pobrania w zaleznosci od danych SYNOP, KLIMAT, OPAD:
+  if(interval == "miesieczne") {
 
-    if(rzad == "synop"){
+    if(rank == "synop"){
       b[[1]] <- clean_metadata_meteo(paste0(base_url, "dane_meteorologiczne/miesieczne/synop/s_m_d_format.txt"),
-                               rzad = "synop", interwal = "miesieczne")
+                               rank = "synop", interval = "miesieczne")
       b[[2]] <- clean_metadata_meteo(paste0(base_url, "dane_meteorologiczne/miesieczne/synop/s_m_t_format.txt"),
-                               rzad = "synop", interwal = "miesieczne")
+                               rank = "synop", interval = "miesieczne")
     }
 
-    if(rzad == "klimat"){
+    if(rank == "klimat"){
       b[[1]] <- clean_metadata_meteo(paste0(base_url, "dane_meteorologiczne/miesieczne/klimat/k_m_d_format.txt"),
-                               rzad = "klimat", interwal = "miesieczne")
+                               rank = "klimat", interval = "miesieczne")
       b[[2]] <- clean_metadata_meteo(paste0(base_url, "dane_meteorologiczne/miesieczne/klimat/k_m_t_format.txt"),
-                               rzad = "klimat", interwal = "miesieczne")
+                               rank = "klimat", interval = "miesieczne")
     }
 
-    if(rzad == "opad"){
+    if(rank == "opad"){
       b[[1]] <- clean_metadata_meteo(paste0(base_url, "dane_meteorologiczne/miesieczne/opad/o_m_format.txt"),
-                               rzad = "opad", interwal = "miesieczne")
+                               rank = "opad", interval = "miesieczne")
     }
 
   } # koniec MIESIECZNYCH
 
 
   ## rozpoczecie dla danych TERMINOWYCH:
-  if(interwal == "terminowe"){
-    if(rzad == "synop") b[[1]] <- clean_metadata_meteo(paste0(base_url, "dane_meteorologiczne/terminowe/synop/s_t_format.txt"),
-                                                 rzad = "synop", interwal = "terminowe")
-    if(rzad == "klimat") b[[1]] <- clean_metadata_meteo(paste0(base_url, "dane_meteorologiczne/terminowe/klimat/k_t_format.txt"),
-                                                  rzad = "klimat", interwal = "terminowe")
+  if(interval == "terminowe"){
+    if(rank == "synop") b[[1]] <- clean_metadata_meteo(paste0(base_url, "dane_meteorologiczne/terminowe/synop/s_t_format.txt"),
+                                                 rank = "synop", interval = "terminowe")
+    if(rank == "klimat") b[[1]] <- clean_metadata_meteo(paste0(base_url, "dane_meteorologiczne/terminowe/klimat/k_t_format.txt"),
+                                                  rank = "klimat", interval = "terminowe")
   }
 
   return(b)
 }
 
-# metadane(interwal = "dobowe", rzad = "synop")
-# metadane(interwal = "dobowe", rzad = "klimat")
-# metadane(interwal = "dobowe", rzad = "opad")
+# metadane(interval = "dobowe", rank = "synop")
+# metadane(interval = "dobowe", rank = "klimat")
+# metadane(interval = "dobowe", rank = "opad")
