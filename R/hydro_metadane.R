90e8b22b178bae3d320cc82467ba3af232f767e4
unknown <arkadiusz.glogowski@upwr.edu.pl>
2019-04-10 09:54:56 +0200

    name changes

--- R/hydro_metadane.R
+++ R/hydro_metadane.R
@@ -22,16 +22,16 @@ hydro_metadane <- function(interval){
     # dobowe
     address_meta1 <- paste0(base_url, interval, "/codz_info.txt")
     address_meta2 <- paste0(base_url, interval, "/zjaw_info.txt")
-    meta <- list(hydro_clean_metadata(address_meta1, interval),
-                 hydro_clean_metadata(address_meta2, interval))
+    meta <- list(clean_metadata_hydro(address_meta1, interval),
+                 clean_metadata_hydro(address_meta2, interval))
   } else if (interval == "miesieczne"){
     #miesieczne
     address_meta <- paste0(base_url, interval, "/mies_info.txt")
-    meta <- hydro_clean_metadata(address_meta, interval)
+    meta <- clean_metadata_hydro(address_meta, interval)
   } else if (interval == "polroczne_i_roczne"){
     # polroczne_i_roczne
     address_meta <- paste0(base_url, interval, "/polr_info.txt")
-    meta <- hydro_clean_metadata(address_meta, interval)
+    meta <- clean_metadata_hydro(address_meta, interval)
   } else{
     stop("Wrong `interval` value. It should be either 'dobowe', 'miesieczne', or 'polroczne_i_roczne'.")
   }
