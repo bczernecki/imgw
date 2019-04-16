c1d5d62f2b383e1dc401056a0138bb3145aa3e2a
Nowosad <tupiszakaczki@gmail.com>
2019-04-16 11:35:15 +0200

    cleans hydro tests

--- tests/testthat/test-hydro-metadata.R
+++ tests/testthat/test-hydro-metadata.R
@@ -1,7 +1,9 @@
 context("hydro-metadata")
 
+h_d <- hydro_metadata("daily")
+h_m <- hydro_metadata("monthly")
+h_a <- hydro_metadata("semiannual_and_annual")
+
 test_that("hydro-metadata works!", {
-  m <- hydro_metadata("daily")
-  m <- hydro_metadata("monthly")
-  m <- hydro_metadata("semiannual_and_annual")
+
 })
