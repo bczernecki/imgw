1385c3777ee8ada39c4a1e916cfa6b45cc7cc2ec
unknown <arkadiusz.glogowski@upwr.edu.pl>
2019-04-19 12:39:22 +0200

    add station selection

--- R/hydro.R
+++ R/hydro.R
@@ -5,23 +5,25 @@
 #' @param interval temporal resolution of the data ("daily" , "monthly", or "semiannual_and_annual")
 #' @param year vector of years (e.g., 1966:2000)
 #' @param value type of data (can be: state - "H" (default), flow - "Q", or temperature - "T")
+#' @param station vector of hydrological stations danepubliczne.imgw.pl can be name of station CAPITAL LETTERS(character)
+#' or ID of station(numeric)
 #' @export
 #'
 #' @examples \dontrun{
 #'   x <- hydro("monthly", year = 1999)
 #'   head(x)
 #' }
-hydro <- function(interval, year, value = NULL){
+hydro <- function(interval, year, value = NULL,station=F){
 
   if (interval == "daily"){
     # dobowe
-    calosc <- hydro_daily(year = year)
+    calosc <- hydro_daily(year = year,station = station)
   } else if (interval == "monthly"){
     #miesieczne
-    calosc <- hydro_monthly(year = year)
+    calosc <- hydro_monthly(year = year,station = station)
   } else if (interval == "semiannual_and_annual"){
     # polroczne_i_roczne
-    calosc <- hydro_annual(year = year, value = value)
+    calosc <- hydro_annual(year = year, value = value,station = station)
   } else{
     stop("Wrong `interval` value. It should be either 'daily', 'monthly', or 'semiannual_and_annual'.", call. = FALSE)
   }
