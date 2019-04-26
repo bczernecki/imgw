14c348a755c1abda717bf6fa3b62896b5b878d65
Bartosz <nwp@amu.edu.pl>
2019-04-26 15:42:19 +0200

    abbreviations

---
+++ R/abbrev.R
@@ -0,0 +1,46 @@
+#' Shortening column names
+#'
+#' Shortening column names to improve readability of downloaded dataset and removing duplicated column names
+#'
+#' @param data downloaded dataset with original column names
+#' @param duplicates whether to remove duplicated column names (default TRUE - i.e. columns with duplicated names are deleted)
+#' @param format three types of column names possible: "short" - default, values with shorten names, "polish" - (original names as downloaded), "full" - full English description
+#' @importFrom utils download.file unzip read.csv
+#' @export
+#'
+#' @examples \dontrun{
+#'   monthly <- meteo_monthly(rank = "climate", year = 1969)
+#'   head(monthly)
+#' }
+#'
+
+abbrev <- function(data, duplicates = TRUE, format = "short"){
+
+  data <- meteo_daily("synop", year=2010)
+  head(data)
+  abbrev <- readRDS("data/abbrev.rda")
+  str(abbrev)
+  orig_columns <- trimws(gsub("\\s+", " ", colnames(data))) # remove double spaces
+
+  if(format == "short"){
+    # abbrev english
+    colnames(data) <- abbrev$abbr_ang[match(orig_columns, abbrev$fullname)]
+  }
+
+  if(format == "full"){
+    # full english names:
+    colnames(data) <- abbrev$fullname_ang[match(orig_columns, abbrev$fullname)]
+  }
+
+  if(format == "polish"){
+    # fullname polish, no changes required:
+    # abbrev$fullname[match(orig_columns, abbrev$fullname)]
+  }
+
+
+  # removing duplicated column names:  (e.g. station's name)
+  if(duplicates == TRUE){
+  data <- data[,!duplicated(colnames(data))]
+  }
+
+} # end of function
--- data-raw/parametry_przyklad_synop.R
+++ data-raw/parametry_przyklad_synop.R
@@ -1,27 +1,25 @@
 library(imgw)
+library(stringr)
 synop <- meteo_daily("synop", year=2010)
+daily <- synop
 head(daily)
 
 abbrev <- read.csv("data-raw/parametry_skrot.csv", stringsAsFactors = F)
 
 orig_columns <- trimws(gsub("\\s+", " ", colnames(daily))) # remove double spaces
 
-# fullname:
+# fullname polish, no changes required:
 abbrev$fullname[match(orig_columns, abbrev$fullname)]
 
-abbrev$abbr_ang[match(orig_columns, abbrev$fullname)]
-
-merge(orig_columns, data2,
-      by = c("Kod stacji", "Rok", "Miesiac"),
-      all.x = TRUE)
-
-nazwy <- data.frame(fullname = as.character(str_squish(colnames(synop))), stringsAsFactors = F) #usuwa podwojne spacje, etc.
-
-
-
-colnames(synop) <- dplyr::left_join(nazwy, abbrev)$abbr_ang
+# abbrev english
+colnames(synop) <- abbrev$abbr_ang[match(orig_columns, abbrev$fullname)]
 head(synop)
 
-#plot(synop$t2m_daily_mean,synop$t2m_daily_mean
-colnames(synop)
+# fullname english
+colnames(synop) <- abbrev$fullname_ang[match(orig_columns, abbrev$fullname)]
+head(synop)
+
+
 # zastanowic sie nad usunieciem zduplikowanych kolumn (Np. nazwa stacji)
+synop <- synop[,!duplicated(colnames(synop))]
+head(synop)
