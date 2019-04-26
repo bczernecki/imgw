4389bfeba59f79506656d3dc5a2cc62c9fef36c3
Nowosad <tupiszakaczki@gmail.com>
2019-04-26 21:57:01 +0200

    replaces format with col_names

--- R/shortening.R
+++ R/shortening.R
@@ -3,32 +3,32 @@
 #' Shortening column names to improve the readability of downloaded dataset and removing duplicated column names
 #'
 #' @param data downloaded dataset with original column names
-#' @param format three types of column names possible: "short" - default, values with shorten names, "polish" - (original names as downloaded), "full" - full English description
+#' @param col_names three types of column names possible: "short" - default, values with shorten names, "full" - full English description, "polish" - original names in the dataset
 #' @param duplicates whether to remove duplicated column names (default TRUE - i.e., columns with duplicated names are deleted)
 #' @export
 #'
 #' @examples \dontrun{
 #'   colnames(data)
-#'   abbr <- shortening(data = data, format = "short", duplicates = TRUE)
+#'   abbr <- shortening(data = data, colnames = "short", duplicates = TRUE)
 #'   head(abbr)
 #' }
 #'
 
-shortening <- function(data, format = "short", duplicates = TRUE){
+shortening <- function(data, col_names = "short", duplicates = TRUE){
 
-  if (format != "polish"){
+  if (col_names != "polish"){
     abbrev <- imgw::abbrev
     orig_columns <- trimws(gsub("\\s+", " ", colnames(data))) # remove double spaces
 
     matches <- match(orig_columns, abbrev$fullname)
     matches <- matches[!is.na(matches)]
 
-    if (format == "short"){
+    if (col_names == "short"){
       # abbrev english
       colnames(data)[orig_columns %in% abbrev$fullname] <- abbrev$abbr_ang[matches]
     }
 
-    if (format == "full"){
+    if (col_names == "full"){
       # full english names:
       colnames(data)[orig_columns %in% abbrev$fullname] <- abbrev$fullname_ang[matches]
     }
