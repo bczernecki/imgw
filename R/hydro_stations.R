a19ef9d4e1db1db190e502059e89dd2f7060d174
Nowosad <tupiszakaczki@gmail.com>
2019-04-26 13:42:00 +0200

    adds hydro sites and improves meteo sites

--- R/hydro_stations.R
+++ R/hydro_stations.R
@@ -1,21 +1,20 @@
-#' @name stacje_meteo
-#' @title Location of the weather stations
+#' @name hydro_stations
+#' @title Location of the hydrological stations
 #'
 #' @description The object contains weather stations
 #' coordinates, ID numbers, and elevations
 #'
-#' @format The data contains a data.frame with 1998 obs. of 4 variables:
+#' @format The data contains a data.frame with 1304 obs. of 3 variables:
 #' \itemize{
+#'     \item{id} {Station ID}
 #'     \item{X} {Longitude}
 #'     \item{Y} {Latitude}
-#'     \item{Kod_stacji} {Station ID}
-#'     \item{Wysokosc} {Station elevation in m asl}
 #' }
 #' The object is in the geographic coordinates using WGS84 (EPSG:4326).
 #'
 #' @docType data
 #' @keywords datasets meteo
 #' @examples
-#' data(stacje_meteo)
-#' head(stacje_meteo)
-"stacje_meteo"
+#' data(hydro_stations)
+#' head(hydro_stations)
+"hydro_stations"
