7085d1b9e406875adea79f5451893335f25ac079
Nowosad <tupiszakaczki@gmail.com>
2019-04-27 12:45:40 +0200

    adds hydro metadata tests

--- tests/testthat/test-hydro-metadata.R
+++ tests/testthat/test-hydro-metadata.R
@@ -5,5 +5,10 @@ h_m <- hydro_metadata("monthly")
 h_a <- hydro_metadata("semiannual_and_annual")
 
 test_that("hydro-metadata works!", {
-
+  expect_equal(dim(h_d[[1]]), c(10, 1))
+  expect_equal(dim(h_d[[2]]), c(10, 1))
+  expect_equal(dim(h_m[[1]]), c(10, 1))
+  expect_equal(dim(h_a[[1]]), c(16, 1))
+  expect_equal(dim(h_a[[2]]), c(16, 1))
+  expect_equal(dim(h_a[[3]]), c(16, 1))
 })
