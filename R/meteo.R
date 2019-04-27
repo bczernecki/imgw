f18568919a57fc7ce22d8f4d16a27f0df2f3e104
Nowosad <tupiszakaczki@gmail.com>
2019-04-27 11:47:24 +0200

    updates meteo to the new api

--- R/meteo.R
+++ R/meteo.R
@@ -7,7 +7,7 @@
 #' @param year vector of years (e.g., 1966:2000)
 #' @param status leave the columns with measurement and observation statuses (default status = FALSE - i.e. the status columns are deleted)
 #' @param coords add coordinates of the station (logical value TRUE or FALSE)
-#' @param short shortening column names (logical value TRUE or FALSE)
+#' @param col_names three types of column names possible: "short" - default, values with shorten names, "full" - full English description, "polish" - original names in the dataset
 #' @param ... other parameters that may be passed to 'shortening' function that shortens column names
 
 #' @export
@@ -16,20 +16,19 @@
 #'   x <- meteo("monthly", rank = "synop", year = 2018, coords = TRUE)
 #'   head(x)
 #' }
-meteo <- function(interval, rank, year, status = FALSE, coords = FALSE, short = TRUE, ...){
+meteo <- function(interval, rank, year, status = FALSE, coords = FALSE, col_names = "short", ...){
   if (interval == "daily"){
     # daily
-    calosc <- meteo_daily(rank = rank, year = year, status = status, coords = coords, short = short, ...)
+    calosc <- meteo_daily(rank = rank, year = year, status = status, coords = coords, col_names = col_names, ...)
   } else if (interval == "monthly"){
     #monthly
-    calosc <- meteo_monthly(rank = rank, year = year, status = status, coords = coords, short = short, ...)
+    calosc <- meteo_monthly(rank = rank, year = year, status = status, coords = coords, col_names = col_names, ...)
   } else if (interval == "hourly"){
     #hourly
-    calosc <- meteo_hourly(rank = rank, year = year, status = status, coords = coords, short = short, ...)
+    calosc <- meteo_hourly(rank = rank, year = year, status = status, coords = coords, col_names = col_names, ...)
   } else{
     stop("Wrong `interval` value. It should be either 'hourly', daily', or 'monthly'.")
   }
-
 
   return(calosc)
 }
