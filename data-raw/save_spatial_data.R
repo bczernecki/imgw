4ce2ea0b9caa1ab414560668630f5f6f786a0c10
Nowosad <tupiszakaczki@gmail.com>
2019-04-27 13:13:20 +0200

    fixes Nan to NA

--- data-raw/save_spatial_data.R
+++ data-raw/save_spatial_data.R
@@ -3,5 +3,5 @@ library(readr)
 hydro_stations = read_csv("data-raw/hydro_stations.csv")
 meteo_stations = read_csv("data-raw/meteo_stations.csv")
 
-usethis::use_data(hydro_stations)
-usethis::use_data(meteo_stations)
+usethis::use_data(hydro_stations, overwrite = TRUE)
+usethis::use_data(meteo_stations, overwrite = TRUE)
