595b482b8cf7358a56e94893f5eba306896c5653
unknown <arkadiusz.glogowski@upwr.edu.pl>
2020-01-21 20:57:17 +0100

    data for improvment of synoptic station seletion

---
+++ R/synop_stations.R
@@ -0,0 +1,17 @@
+#' @name synop_stations
+#' @title synop stations for RAM saving
+#'
+#' @description The object contains , ID numbers, and names of synoptic station in Poland
+#'
+#' @format The data contains a data.frame with 66 obs. of 2 variables:
+#' \itemize{
+#'     \item{id} {Station ID}
+#'     \item{station} {Station name}
+#' }
+#'
+#' @docType data
+#' @keywords datasets meteo
+#' @examples
+#' data(synop_stations)
+#' head(synop_stations)
+"synop_stations"
